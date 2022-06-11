5b129d2d6ac6939a73ace20877adbe4022d9a409
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-06-12 02:11:34 +0530

    update login flow

--- pages/login.js
+++ pages/login.js
@@ -1,141 +1,141 @@
 import React, { useState } from 'react';
 import Card from '@mui/material/Card';
-import TextField from "@mui/material/TextField";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { auth } from "../firebase/config";
-import { Box } from "@mui/material";
-import { Button } from "../components/Button";
+import { RecaptchaVerifier } from 'firebase/auth';
+import { auth } from '../firebase/config';
+import { useAuth } from '../context/AuthUserContext';
 
 const Login = () => {
-  const [mynumber, setnumber] = useState("");
-  const [otp, setotp] = useState("");
-  const [show, setshow] = useState(false);
-  const [final, setfinal] = useState("");
+	const [mynumber, setnumber] = useState('');
+	const [otp, setotp] = useState('');
+	const [show, setshow] = useState(false);
+	const [final, setfinal] = useState('');
 
-  const signin = () => {
-    if (mynumber === "" || mynumber.length < 10) return;
+	const { signInPhone, authUser } = useAuth();
 
-    let verify = new RecaptchaVerifier(
-      "recaptcha-container",
-      {
-        size: "normal",
-        callback: (response) => {
-          // reCAPTCHA solved, allow signInWithPhoneNumber.
-          // .../
-        },
-        "expired-callback": () => {
-          // Response expired. Ask user to solve reCAPTCHA again.
-          // ...
-        },
-      },
-      auth
-    );
+	const signin = () => {
+		if (mynumber === '' || mynumber.length < 10) return;
 
-    signInWithPhoneNumber(auth, mynumber, verify)
-      .then((result) => {
-        setfinal(result);
-        alert("code sent");
-        setshow(true);
-      })
-      .catch((err) => {
-        alert(err);
-        window.location.reload();
-      });
-  };
+		let verify = new RecaptchaVerifier(
+			'recaptcha-container',
+			{
+				size: 'invisible',
+				callback: (response) => {
+					// reCAPTCHA solved, allow signInWithPhoneNumber.
+					// .../
+				},
+				'expired-callback': () => {
+					// Response expired. Ask user to solve reCAPTCHA again.
+					// ...
+				},
+			},
+			auth
+		);
 
-  const ValidateOtp = () => {
-    if (otp === null || final === null) return;
-    final
-      .confirm(otp)
-      .then((result) => {
-        // success
-        console.log("otp verified");
-      })
-      .catch((err) => {
-        alert("Wrong code");
-      });
-  };
+		signInPhone(mynumber, verify)
+			.then((res) => {
+				setshow(true);
+				setfinal(res);
+			})
+			.catch((err) => {
+				window.location.reload();
+			});
+	};
 
-  return (
-    // <div style={{ marginTop: '200px' }}>
-    // 	<center>
-    // 		<div style={{ display: !show ? 'block' : 'none' }}>
-    // 			<input
-    // 				value={mynumber}
-    // 				onChange={(e) => {
-    // 					setnumber(e.target.value);
-    // 				}}
-    // 				placeholder='phone number'
-    // 			/>
-    // 			<br />
-    // 			<br />
-    // 			<div id='recaptcha-container'></div>
-    // 			<button onClick={signin}>Send OTP</button>
-    // 		</div>
-    // 		<div style={{ display: show ? 'block' : 'none' }}>
-    // 			<input
-    // 				type='text'
-    // 				placeholder={'Enter your OTP'}
-    // 				onChange={(e) => {
-    // 					setotp(e.target.value);
-    // 				}}
-    // 			></input>
-    // 			<br />
-    // 			<br />
-    // 			<button onClick={ValidateOtp}>Verify</button>
-    // 		</div>
-    // 	</center>
-    // </div>
-    <Box
-      sx={{
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <img src="/logo.svg" width={300} />
+	const ValidateOtp = () => {
+		if (otp === null || final === null) return;
+		final
+			.confirm(otp)
+			.then((result) => {
+				// success
+				console.log('otp verified');
+			})
+			.catch((err) => {
+				alert('Wrong code');
+			});
+	};
 
-      <Card
-        sx={{
-          margin: "25px",
-          width: "80%",
-          maxWidth: "400px",
-          height: "400px",
-          padding: "25px",
-          display: "flex",
-          justifyContent: "center",
-          "flex-direction": "column",
-        }}
-      >
-        <h1>Hi, There</h1>
-        <TextField
-          id="filled-basic"
-          label="Your Phone Number"
-          variant="filled"
-          value={mynumber}
-          onChange={(e) => {
-            setnumber(e.target.value);
-          }}
-        />
-        <TextField
-          sx={{ marginTop: "15px" }}
-          id="filled-basic"
-          label="Enter OTP"
-          variant="filled"
-          value={otp}
-          onChange={(e) => {
-            setnumber(e.target.value);
-          }}
-        />
+	return (
+		<div style={{ marginTop: '200px' }}>
+			<center>
+				<div style={{ display: !show ? 'block' : 'none' }}>
+					<input
+						value={mynumber}
+						onChange={(e) => {
+							setnumber(e.target.value);
+						}}
+						placeholder='phone number'
+					/>
+					<br />
+					<br />
+					<div id='recaptcha-container'></div>
+					<button onClick={signin}>Send OTP</button>
+				</div>
+				<div style={{ display: show ? 'block' : 'none' }}>
+					<input
+						type='text'
+						placeholder={'Enter your OTP'}
+						onChange={(e) => {
+							setotp(e.target.value);
+						}}
+					></input>
+					<br />
+					<br />
+					<button onClick={ValidateOtp}>Verify</button>
+				</div>
+			</center>
+		</div>
+		// <div
+		// 	sx={{
+		// 		height: '100%',
+		// 		display: 'flex',
+		// 		alignItems: 'center',
+		// 		justifyContent: 'center',
+		// 	}}
+		// >
+		// 	<Card
+		// 		sx={{
+		// 			margin: '25px',
+		// 			width: '400px',
+		// 			height: '400px',
+		// 			padding: '25px',
+		// 			display: 'flex',
+		// 			justifyContent: 'center',
+		// 			'flex-direction': 'column',
+		// 		}}
+		// 	>
+		// 		<h1>Hi, There</h1>
+		// 		<TextField
+		// 			id='filled-basic'
+		// 			label='Your Phone Number'
+		// 			variant='filled'
+		// 			value={mynumber}
+		// 			onChange={(e) => {
+		// 				setnumber(e.target.value);
+		// 			}}
+		// 		/>
+		// 		<TextField
+		// 			sx={{ marginTop: '15px' }}
+		// 			id='filled-basic'
+		// 			label='Enter OTP'
+		// 			variant='filled'
+		// 			value={otp}
+		// 			onChange={(e) => {
+		// 				setnumber(e.target.value);
+		// 			}}
+		// 		/>
 
-        <Button sx={{ marginTop: "20px" }}>Send OTP</Button>
-        <Button sx={{ marginTop: "20px" }}>Verify OTP</Button>
-      </Card>
-    </Box>
-  );
+		// 		<Button sx={{ marginTop: '20px' }} variant='contained'>
+		// 			Send OTP
+		// 		</Button>
+		// 		<Button sx={{ marginTop: '20px' }} variant='contained'>
+		// 			Verify OTP
+		// 		</Button>
+		// 	</Card>
+		// </div>
+	);
 };
 
 export default Login;
