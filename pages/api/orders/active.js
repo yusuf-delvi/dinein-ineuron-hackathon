ce2f8c61c71a96a8b83d273ddbd1bde527231cce
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-06-11 17:52:59 +0530

    orders api

---
+++ pages/api/orders/active.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { firestore } from '../../../firebase/config';
+
+import {
+	collection,
+	QueryDocumentSnapshot,
+	DocumentData,
+	query,
+	where,
+	limit,
+	getDocs,
+} from '@firebase/firestore';
+
+export default async function hello(req, res) {
+	const { userid } = req.query;
+
+	// add where userid
+	const activeOrdersSnapshot = await getDocs(
+		collection(firestore, 'activeOrders')
+	);
+
+	const orders = [];
+
+	activeOrdersSnapshot.forEach((doc) => {
+		orders.push({ _id: doc.id, ...doc.data() });
+	});
+
+	res.status(200).json({ orders });
+}
