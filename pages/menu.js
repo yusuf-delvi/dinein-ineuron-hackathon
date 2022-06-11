91d12da98259a810fc93ba77f106c0d0b5ff5aab
nawaz-cumin <96826800+nawaz-cumin@users.noreply.github.com>
2022-06-11 18:29:22 +0530

    Menu List Page

--- pages/menu.js
+++ pages/menu.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
+import styles from '../styles/Menu.module.css';
+import Button from '@mui/material/Button';
 
 const MenuItem = () => {
-	return <div>menu item</div>;
+	return <div sx={{ marginTop: '30px' }} >
+		<Card className={styles.menuItemContainer}>
+			<div className={styles.menuItemsimg}>
+			<img src='/pizza.jpg'>
+			</img>
+			</div>
+			
+			<div className={styles.menuItemsmain}>
+				<h1>
+					Cheese Pizza
+				</h1>
+				<div className={styles.menuItemssub}>
+					<h3 >$10.00</h3>
+					<Chip sx={{marginLeft:"20px"}} label="Vegan"></Chip>
+				</div>
+			</div>
+
+			<div className={styles.menuItemBtnContainer}>
+			<Button variant="contained">Add to Cart</Button>
+			</div>
+
+		</Card>
+	</div>;
 };
 
 const Menu = () => {
 	return (
-		<>
-			<div>Menu</div>
-
+		<div className={styles.menuContainer}>
+			<div>Explore Our Menu</div>
 			<MenuItem />
-		</>
+			<MenuItem />
+			<MenuItem />
+		</div>
 	);
 };
 
