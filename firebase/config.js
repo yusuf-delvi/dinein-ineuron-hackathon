bce29561251c8889011492ee0c4de5833da69d1a
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-06-11 13:14:03 +0530

    firebase init

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVgsqueF8ZICSqnZuaAbZoyg3-YM6sLYE",
+  authDomain: "hackathon-ineuron.firebaseapp.com",
+  projectId: "hackathon-ineuron",
+  storageBucket: "hackathon-ineuron.appspot.com",
+  messagingSenderId: "260892220848",
+  appId: "1:260892220848:web:095cefaa4c56091f7c0a75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+export {firestore}
