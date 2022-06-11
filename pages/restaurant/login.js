e6e4d6020525d3391c083bfb3ac0350e6096f44e
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-06-11 11:47:33 +0530

    add nextjs with docker

---
+++ pages/restaurant/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div> 
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+        <button>Login</button>
+        </div>
+   
+  )
+}
+
+export default login
