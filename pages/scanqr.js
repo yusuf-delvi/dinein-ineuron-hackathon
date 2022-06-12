020c0a5847de3904159abb924cb809a123fa3b9d
codewaseem <codewaseem@gmail.com>
2022-06-12 10:27:31 +0530

    push

--- pages/scanqr.js
+++ pages/scanqr.js
@@ -3,32 +3,36 @@ import { QrReader } from 'react-qr-reader';
 import { Box } from '@mui/material'
 import { useRouter} from 'next/router'
 const Scanqr = () => {
-	return (
-		<div>
-			<Box sx={{
-				textAlign:'center'
-			}}>
-			<h1 >Scan QR</h1>
-			<p>Scan to Place Order</p>
-			</Box>
-			
-			<QrReader
-				
-				constraints={{facingMode: "environment"}}
-				onResult={(result, error) => {
-					if (!!result) {
-						console.log(result)
-						setData(result?.text);
-					}
 
-					if (!!error) {
-						console.info(error);
-					}
-				}}
-				style={{ width: '100%' }}
-			/>
-		</div>
-	);
+	const { push } = useRouter();
+  return (
+    <div>
+      <Box
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        <h1>Scan QR</h1>
+        <p>Scan to Place Order</p>
+      </Box>
+
+      <QrReader
+        constraints={{ facingMode: "environment" }}
+        onResult={(result, error) => {
+          if (!!result) {
+            console.log(result);
+            setData(result?.text);
+            push(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default Scanqr;
